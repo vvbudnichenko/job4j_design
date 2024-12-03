da18e4fec1d89603367e0a8ed4988d0533bd2bc9
vvbudnichenko <budnichik@gmail.com>
2024-12-03 18:56:06 +0200

    check

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("New project");
+        System.out.println("New project2");
     }
 }
