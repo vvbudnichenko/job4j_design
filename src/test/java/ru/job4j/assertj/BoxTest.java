518d56ee44f7bf0ea824886ce7d1a50f436ae6c6
vvbudnichenko <budnichik@gmail.com>
2024-12-04 18:16:37 +0200

    3. Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isTetrahedron() {
+        Box box = new Box(4, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isCube() {
+        Box box = new Box(8, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void isUnknown() {
+        Box box = new Box(12, 12);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Unknown object");
+    }
+}
